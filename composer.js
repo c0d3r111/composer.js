5aabbd2ad2b339086fec4d38e151a6dcf09a2f04
Reheem Carmichael <25629615+c0d3r111@users.noreply.github.com>
2021-08-08 16:29:54 +0000

    Update composer.js

--- composer.js
+++ composer.js
@@ -6,32 +6,20 @@
 **/
 
 void function() {
-    class Creator  {
-        constructor() {
-            const tags = [
-                "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"
-            ];
+    window.Create   = {
+        node : function() {
+            if (!(this instanceof String)) return;
 
-            void tags.forEach(tag => {
-                void Object.defineProperty(this, tag, {
-                    get: this.node.bind(Object.create(null, { tag: { value: tag } }))
-                });
-            });
-        }
-        node() {
-            if (!this.tag) return;
+            const obj   = Object.create(window.Composer);
 
-            const obj   = Object.create(window.composer);
-
-            obj.element = document.createElement(this.tag);
-            obj.data    = Object.create(null);
+            obj.element = document.createElement(this);
 
             return obj;
-        };
-        raw(subject) {
-            if (!subject) return create.div;
+        },
+        raw  : function(subject) {
+            if (!subject) return this.div;
 
-            const node = Object.create(window.composer);
+            const node = Object.create(window.Composer);
 
             if (typeof subject === 'string') {
                 if (subject[0] === '<') {
@@ -52,13 +40,15 @@ void function() {
 
             return node;
         }
-    }
-    class Composer {
-        constructor() {
-            this.store = Object.create(null);
-        }
-        add(nodes) {
-            if (!nodes) return this;
+    };
+    window.Composer = {
+        get isVisible() {
+            return Boolean(this.element)
+                ? this.element.style.display !== 'none'
+                : false;
+        },
+
+        add        : function(nodes) {
             if (nodes instanceof Array) {
                 const fragment = document.createDocumentFragment();
 
@@ -86,85 +76,115 @@ void function() {
             }
 
             return this;
-        }
-        alt(text) {
-            void this.element.setAttribute('alt', String(text));
-
-            return this;
-        }
-        append(text) {
+        },
+        append     : function(text) {
             this.element.innerText = this.element.innerText + (text || "");
 
             return this;
-        }
-        attr(attributes) {
+        },
+        attr       : function(attributes) {
             if (attributes instanceof Object) {
                 void Object.keys(attributes).forEach(attr => {
-                    if (!attr) return;
-
-                    !attributes[attr]
+                    return !attributes[attr]
                         ? void this.element.removeAttribute(attr)
                         : void this.element.setAttribute(attr, attributes[attr]);
                 });
             }
 
             return this;
-        }
-        clear() {
+        },
+        clear      : function() {
             while (this.element.firstChild) {
                 void this.element.removeChild(this.element.firstChild);
             }
 
             return this;
-        }
-        click(method) {
+        },
+        click      : function(method) {
             if (method instanceof Function) {
                 this.element.onclick = method;
             }
 
             return this;
-        }
-        copy() {
+        },
+        copy       : function() {
             const obj = Object.create(this);
 
             obj.element         = this.element.cloneNode(true);
             obj.element.onclick = this.element.onclick;
 
             return obj;
-        }
-        event(name, method) {
+        },
+        declass    : function(name) {
+            Boolean(name)
+                ? void name.split(' ').forEach(item => void this.element.classList.remove(item))
+                : void 0;
+
+            return this;
+        },
+        edit       : function() {
+            void this.element.setAttribute('contentEditable', true);
+
+            return this;
+        },
+        emit       : function(name) {
+            if (name) {
+                void window.dispatchEvent(new Event(name));
+            }
+
+            return this;
+        },
+        event      : function(name, method) {
             if (name && method) {
                 void this.element.addEventListener(name, method);
             }
 
             return this;
-        }
-        fadeout() {
-            this.element.style.transition = 'all 0.5s';
-            this.element.style.opacity    = '0';
+        },
+        flash      : function(message) {
+            const currentText = this.element.value || this.element.innerText;
+
+            this.element.value     = message;
+            this.element.innerText = message;
 
             void setTimeout(() => {
-                this.element.style.display = 'none';
-                this.element.style.opacity = '1';
-            }, 450);
+                this.element.value     = currentText;
+                this.element.innerText = currentText;
+            }, 2500);
 
             return this;
-        }
-        hide(conditional) {
+        },
+        hasChild   : function(name) {
+            let id = this.store[name]; 
+
+            if (!id) return false;
+
+            for (const child of this.element.childNodes) {
+                if (child.id === id) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+        hasClass   : function(name) {
+            return this.element.classList.contains(name);
+        },
+        hide       : function(conditional) {
             if (!conditional) {
                 this.element.style.display = 'none';
             }
 
             return this;
-        }
-        html(data) {
+        },
+        html       : function(data) {
             if (typeof data === 'string') {
                 this.element.innerHTML = data;
             }
 
             return this;
-        }
-        id(name) {
+        },
+        id         : function(name) {
             if (name) {
                 this.store[name] = this.element.id || this.store[name] || this.uid();
                 this.element.id  = this.store[name];
@@ -173,8 +193,13 @@ void function() {
             }
 
             return this;
-        }
-        link(url) {
+        },
+        insert     : function(node) {
+            void this.element.insertBefore(node.element || node, this.element.firstChild);
+
+            return this;
+        },
+        link       : function(url) {
             const tag = this.element.tagName;
 
             tag === 'A' || tag === 'LINK'
@@ -182,161 +207,131 @@ void function() {
                 : void this.element.setAttribute('src', url);
 
             return this;
-        }
-        names(names) {
-            if (names) {
-                void this.element.setAttribute('class', names);
-            }
+        },
+        names      : function(names) {
+            void this.element.setAttribute('class', names || '');
 
             return this;
-        }
-        navigate(url) {
+        },
+        navigate   : function(url, blank) {
             if (url) {
                 this.element.onclick = function () {
-                    window.location.href = this.url;
+                    return this.blank
+                        ? void window.open(this.url, '_blank')
+                        : void (window.location.href = this.url);
                 }.bind({
-                    url: String(url)
+                    url   : String(url),
+                    blank : Boolean(blank)
                 });
             }
 
             return this;
-        }
-        on(event, method) {
+        },
+        on         : function(event, method) {
             if (method instanceof Function) {
                 this.element["on" + event] = method;
             }
 
             return this;
-        }
-        remove(delay) {
-            void setTimeout(() => this.element.remove(), delay || 0);
+        },
+        reclass    : function(name) {
+            Boolean(name)
+                ? void name.split(' ').forEach(item => void this.element.classList.add(item))
+                : void 0;
 
             return this;
-        }
-        show(type) {
-            this.element.style.display = type || 'block';
+        },
+        remove     : function(delay) {
+            Boolean(delay)
+                ? void setTimeout(() => this.element.remove(), delay)
+                : void this.element.remove();
 
             return this;
-        }
-        state(name, clear) {
+        },
+        show       : function(type, delay) {
+            Boolean(delay)
+                ? void setTimeout(() => this.element.style.display = type || 'block', delay)
+                : void (this.element.style.display = type || 'block');
+
+            return this;
+        },
+        state      : function(name, clear) {
             void (
                 clear
                     ? this.element.removeAttribute(name)
-                    : this.element.setAttribute(name, 'true')
+                    : this.element.setAttribute(name, true)
             );
 
             return this;
-        }
-        style(style, delay) {
-            if (!delay) {
-                void Object.keys(style).forEach(property => {
-                    if (property) this.element.style[property] = style[property];
-                });
+        },
+        style      : function(style, delay) {
+            if (style instanceof Object) {
+                if (!delay) {
+                    void Object.keys(style).forEach(property => {
+                        if (property) this.element.style[property] = style[property];
+                    });
 
-                return this;
+                    return this;
 
+                }
+
+                void setTimeout(() => void this.style(style), delay);
             }
 
-            return void setTimeout(() => void this.style(style), delay);
-        }
-        submit(method) {
+            return this;
+        },
+        submit     : function(method) {
             if (method instanceof Function) {
                 this.element.onsubmit = method;
             }
 
             return this;
-        }
-        text(text) {
+        },
+        text       : function(text) {
             this.element.innerText = String(text);
 
             return this;
-        }
-        toggle(name) {
+        },
+        toggle     : function(name) {
             if (name) {
                 void this.element.classList.toggle(String(name));
             }
 
             return this;
-        }
-        uid() {
+        },
+        transition : function(prop, value, duration, timing) {
+            !Boolean(duration)
+                ? void 0
+                : void this.style({
+                    transition : `${prop || 'all'} ${duration || '0.5'}s ${timing || 'ease-in-out'}`,
+                    [prop]     : value || 0,
+                });
+
+            return this;
+        },
+        uid        : function() {
             return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
                 + Math.random().toString(16).slice(2)
                 + String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-        }
-        value(value) {
-            if (value || value === '0' || value === 0) {
-                this.element.value = String(value);
-            }
-            else {
-                this.element.value = '';
-            }
+        },
+        value      : function(value) {
+            this.element.value = String(value == 0 ? value : Boolean(value) ? value : '');
 
             return this;
-        }
-    }
-    class Styler   {
-        media(sheet, obj, name, id) {
-            void sheet.push(name + '{');
-            void Object.keys(obj).forEach(prop => void this.rule(sheet, obj[prop], prop, id));
-            void sheet.push('}');
-        }
-        inner(sheet, obj, name, id) {
-            void sheet.push(name + '{');
-            void this.rule(sheet, obj, id);
-            void sheet.push('}');
-        }
-        rule(sheet, obj, prop, id) {
-            const keys   = Object.keys(obj);
-            const nested = [];
-            const paper  = [];
-            const props  = (prop instanceof Array ? prop : prop.split(','))
-                .map(item => ((id || '') + ' ' + item).trim());
-
-            void keys.forEach((item, index) => {
-                typeof obj[item] === 'object'
-                    ? void nested.push(index)
-                    : void paper.push(item.replace(/_/g, '-') + ':' + obj[item] + ';');
-            });
-
-            if (paper.length) {
-                void sheet.push(props.join(',') + '{');
-                void sheet.push(...paper);
-                void sheet.push('}');
-            }
-
-            void nested.forEach(index => {
-                const focus    = keys[index];
-                const newprops = props
-                    .map(item => (item + (focus[0] === ':' ? focus : ' ' + focus)).trim());
-
-                focus[0] === '@'
-                    ? void this.inner(sheet, obj[focus], focus, newprops.join(',').replace(focus, ''))
-                    : void this.rule(sheet, obj[focus], newprops);
-            });
-        }
-        compile(style, id) {
-            if (!style || typeof style !== 'object') return '';
-
-            const sheet = [];
-
-            void Object.keys(style).forEach(selector => {
-                selector[0] === '@'
-                    ? void this.media(sheet, style[selector], selector, id)
-                    : void this.rule(sheet, style[selector], selector, id);
-            });
-
-            return sheet.join('');
-        }
-    }
-
-    window.styler   = new Styler();
-    window.composer = new Composer();
-    window.create   = new Creator();
-    window.Node     = function(style) {
-        const wrapper = create.div.id();
-
-        return wrapper.add(
-            create.style.text(styler.compile(style, '#' + wrapper.element.id))
-        );
+        },
     };
+
+    window.create         = window.Create;
+    window.composer       = window.Composer;
+    window.Composer.store = Object.create(null);
+
+    const tags = [
+        "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp", "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup", "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr"
+    ];
+
+    void tags.forEach(tag => {
+        void Object.defineProperty(window.Create, tag, {
+            get: window.Create.node.bind(String(tag))
+        });
+    });
 }();
